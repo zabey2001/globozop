de2a1efff2a7b4fc88b13bced7ccc2cd580e4506
Zacharias Ottukallil Pappy <46940852+zabey2001@users.noreply.github.com>
2026-02-28 14:24:50 +0000

    Modify EC2 instance configuration in ec2.tf
    Updated the AMI ID and added tags for the EC2 instance.

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,12 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
 resource "aws_instance" "myec2" {
-    ami = "ami-00c39f71452c08778"
-    instance_type = "t2.micro"
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+
+   tags = {
+    Name = "HelloWorld"
+  }  
 }
